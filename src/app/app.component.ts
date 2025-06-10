e8ae5a5ff489cf4db4a2919730f20209787a14ac
Victor Bruce <victorbruce82@gmail.com>
2025-06-10 18:07:03 +0000

    fix: correct syntax error that prevented the app from starting and logical error for rendering the add cart component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import desseretData from '../../public/data.json';
-import { AddToCartComponent } from "./components/add-to-cart/add-to-cart.component";
+import { AddToCartComponent } from './components/add-to-cart/add-to-cart.component';
 
 @Component({
   selector: 'app-root',
+  imports: [AddToCartComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-});
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'Product list';
+  desserts: Dessert[] | null = null;
+
+  constructor() {
+    this.desserts = desseretData;
+  }
+}
 
 // interface
 interface Dessert {
@@ -14,20 +23,11 @@ interface Dessert {
   name: string;
   category: string;
   price: number;
-};
+}
 
 interface DessertImages {
   thumbnail: string;
   mobile: string;
   tablet: string;
   desktop: string;
-};
-
-export class AppComponent {
-  title = 'Product list';
-  desserts:Dessert[] | null = null;
-
-  constructor() {
-    this.desserts = desseretData;
-  };
-};
+}
--- src/app/components/add-to-cart/add-to-cart.component.ts
+++ src/app/components/add-to-cart/add-to-cart.component.ts
@@ -4,8 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-add-to-cart',
   templateUrl: './add-to-cart.component.html',
   styleUrl: './add-to-cart.component.scss'
-});
-
+})
 export class AddToCartComponent {
   isAddedToCart = false;
   quantity = 1;
