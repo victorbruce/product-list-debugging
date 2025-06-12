56dfe604b7a515119266d1841c8a3cd9175fcda6
Victor Bruce <victorbruce82@gmail.com>
2025-06-12 08:56:21 +0000

    feat: render cart items and add functionality for removing items

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { Dessert } from '../../core/models/dessert.model';
+import { CommonModule } from '@angular/common';
+import { CartService } from '../../services/cart.service';
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
 })
-export class CartComponent {
-  
+export class CartComponent implements OnInit {
+  cartItems: Dessert[] = [];
+  private cartService: CartService = inject(CartService);
+
+  ngOnInit(): void {
+    this.getCartItems();
+  }
+
+  getCartItems() {
+    this.cartService.getCartItems().subscribe((data) => {
+      this.cartItems = data;
+    });
+  }
+  removeItemFromCart(item: Dessert) {
+    this.cartService.removeItemFromCart(item);
+  }
 }
