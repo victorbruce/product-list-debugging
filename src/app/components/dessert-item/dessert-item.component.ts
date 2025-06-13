e8ea4b1bc9c5669014c3805955b5891954a0df72
Victor Bruce <victorbruce82@gmail.com>
2025-06-13 11:20:03 +0000

    feat; mobile responsiveness

--- src/app/components/dessert-item/dessert-item.component.ts
+++ src/app/components/dessert-item/dessert-item.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, inject } from '@angular/core';
 import { AddToCartComponent } from '../add-to-cart/add-to-cart.component';
 import { Dessert } from '../../core/models/dessert.model';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 @Component({
   selector: 'app-dessert-item',
-  imports: [AddToCartComponent],
+  imports: [CommonModule, AddToCartComponent],
   templateUrl: './dessert-item.component.html',
   styleUrl: './dessert-item.component.scss',
 })
